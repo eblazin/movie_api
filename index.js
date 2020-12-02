d2ce470d2e86c0545bb48bdbe32380d3293d83d6
flittle <48164274+eblazin@users.noreply.github.com>
2020-12-02 08:34:09 -0500

    updating post and get request

--- index.js
+++ index.js
@@ -45,9 +45,53 @@ app.get("/movies/directors/:name", (req, res) => {
   );
 });
 
-//Allows user to register
+//Add a user
+/* We'll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthdate: Date
+}*/
+
 app.post("/users", (req, res) => {
-  res.send("Successful POST request returning a username");
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+});
+
+// Get all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Allows users to update their user info(username)
