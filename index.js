f8943146ba9cbd2c36b1994cff201f42433f6a41
flittle <48164274+eblazin@users.noreply.github.com>
2021-03-21 17:31:32 -0400

    removed authentication from movies endpoint

--- index.js
+++ index.js
@@ -45,20 +45,16 @@ app.get("/", (req, res) => {
 });
 
 //Return a list of ALL movies to the user
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 //Return data about a single movie by title to the user
 app.get(
